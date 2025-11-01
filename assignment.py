562a2e681b7fab0f50e7ff136472cdfac739a9c8
Saimraza14 <saimraza1872@gmail.com>
2025-11-01 12:50:55 -0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+fruit = ["appe", "banana", "orange"]
+print(fruit)
+
+thisdict = {
+    "Uni": "Times University Mutan",
+    "Department": "Computer Science",
+    "Name": "Saim Raza",
+    "Reg No": "MC-BSCS-239-015",
+    "Semseter": 5, 
+}
+print(thisdict)
