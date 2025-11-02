340d380b6d17cde87f5e3f5a6fae7ee6e4bf4095
Saimraza14 <saimraza1872@gmail.com>
2025-11-02 11:12:47 -0800

    Refactor assignment.py with additional prints and variables

--- assignment.py
+++ assignment.py
@@ -1,3 +1,17 @@
+print("Hello World", 19)
+# i am learning python
+age=19
+print(age)
+name='Saim'
+print(name)
+print(5+5)
+print("Hello duniya", 18)
+
+fruit=["A","B","C"]
+print(fruit)
+x, y, z = "apple", "banana", "mango"
+print(x, y, z)
+
 fruit = ["appe", "banana", "orange"]
 print(fruit)
 
