bd9e7b5ba9a3766b4822036a34f52bee36e426b5
Saimraza14 <saimraza1872@gmail.com>
2025-11-03 07:37:54 -0800

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,40 @@
+#The is a learning python
+a=1
+print(a)
+b=2
+print(b)
+c=3
+print(c)
+d=4
+print(d)
+e=5
+print(e)
+f=6
+print(f)
+g=7
+print(g)
+h=8
+print(h)
+i=9
+print(i)
+j=10
+print(j)
+
+# true or false
+
+print(5>3)
+print(5==5)
+print(5<10)
+print(5==3)
+a=10
+b=8
+print(a>=b)
+print(a==b)
+print(a<=b)
+
+#How to take input from user in python
+
+name = input("Enter your name: Saim ")
+print("Hello, " + name + "!")
+
+
